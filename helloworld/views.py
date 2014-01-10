643fbeb0e7ca7e5e12c5ab2108fd1fce354dbee7
Evan Yeung <evan.yeung1@gmail.com>
2014-01-10 12:39:11 -0500

    navbar working

--- helloworld/views.py
+++ helloworld/views.py
@@ -4,18 +4,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def welcome(request):
-	return render(request,"helloworld/welcome.html",{})
+	continents = Continent.objects.all()
+	return render(request,"helloworld/welcome.html",{"continents": continents})
 	#return HttpResponse("testing welcome")
 
 def continent(request, continent_id):
+	continents = Continent.objects.all()
 	chosen_continent = Continent.objects.get(pk=continent_id)
 	#return HttpResponse("continent %s" % continent_id)
-	return render(request,"helloworld/continent.html",{"continent":chosen_continent})
+	return render(request,"helloworld/continent.html",{"continent":chosen_continent, "continents": continents})
 
 def country_comment(request,continent_id, country_id):
+	continents = Continent.objects.all()
 	chosen_country = Country.objects.get(pk=country_id)
 	#return HttpResponse("country %s continent %s" % (country_id,continent_id)) 
-	return render(request,"helloworld/country_comment.html",{"country":chosen_country})
+	return render(request,"helloworld/country_comment.html",{"country":chosen_country, "continents": continents})
 
 def get_message(request,continent_id,country_id):
 	chosen_country = Country.objects.get(pk=country_id)
