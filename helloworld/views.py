c7acab66ca4ab038d43334eff6b5a6e1907a7fda
Jeff Zhang <a920249925@gmail.com>
2014-01-10 13:12:20 -0500

    solve the continent country doesn't match problem

--- helloworld/views.py
+++ helloworld/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from helloworld.models import Continent ,Country, Message
 from django.http import HttpResponse
 # Create your views here.
@@ -9,13 +9,16 @@ def welcome(request):
 
 def continent(request, continent_id):
 	chosen_continent = Continent.objects.get(pk=continent_id)
-	#return HttpResponse("continent %s" % continent_id)
 	return render(request,"helloworld/continent.html",{"continent":chosen_continent})
 
+	#return HttpResponse("continent %s" % continent_id)
+
 def country_comment(request,continent_id, country_id):
-	chosen_country = Country.objects.get(pk=country_id)
-	#return HttpResponse("country %s continent %s" % (country_id,continent_id)) 
+	chosen_continent = Continent.objects.get(pk=continent_id)
+	chosen_country = get_object_or_404(chosen_continent.country_set.all(), pk=country.id) 
 	return render(request,"helloworld/country_comment.html",{"country":chosen_country})
+	#return HttpResponse("country %s continent %s" % (country_id,continent_id))
+
 
 def get_message(request,continent_id,country_id):
 	chosen_country = Country.objects.get(pk=country_id)
