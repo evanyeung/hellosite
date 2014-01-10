17eb92ca76fac78caee15c607b35c2248021f509
Jeff Zhang <a920249925@gmail.com>
2014-01-10 11:52:02 -0500

    next change

--- HelloSite/urls.py
+++ HelloSite/urls.py
@@ -8,6 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'HelloSite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^welcome/',include(helloworld.urls)),
+    url(r'^helloworld/',include(helloworld.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
--- helloworld/urls.py
+++ helloworld/urls.py
@@ -4,5 +4,6 @@ from helloworld import views
 urlpatterns = patterns('',
     url(r'^$', views.welcome,name="welcome"),
     url(r'^(?P<continent_id>\d+)/$',views.continent,name="continent"),
-    url(r'^(?P<country_id>\d+)/$',views.country_comment, name = "country_comment"),
+    url(r'^(?P<continent_id>\d+)/(?P<country_id>\d+)/$',views.country_comment, name = "country_comment"),
+    #url(r'^(?P<message_id>\d+)/$',views.message, name = "message"),
     )
