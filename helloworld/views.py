a38cfe07baabbc4354c1d591aa7abbbbe81db185
Jeff Zhang <a920249925@gmail.com>
2014-01-10 12:24:01 -0500

    add message url and view

--- helloworld/views.py
+++ helloworld/views.py
@@ -17,5 +17,11 @@ def country_comment(request,continent_id, country_id):
 	#return HttpResponse("country %s continent %s" % (country_id,continent_id)) 
 	return render(request,"helloworld/country_comment.html",{"country":chosen_country})
 
-def message(request):
-	
+def get_message(request,continent_id,country_id):
+	chosen_country = Country.objects.get(pk=country_id)
+	new_message = Message(author = request.POST["message_author"],
+		message = request.POST["message_message"],country = chosen_country)
+	return HttpResponseRedirect(reverse('country_comment', args=(continent_id,country_id)))
+
+
+
