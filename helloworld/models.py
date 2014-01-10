81eedef3e0461fce7681af794ba980ab53509402
Evan Yeung <evan.yeung1@gmail.com>
2014-01-10 10:46:14 -0500

    updated database

--- helloworld/models.py
+++ helloworld/models.py
@@ -2,15 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Continent(models.Model):
-	name = models.CharField(max_length=10)
+	name = models.CharField(max_length=15)
 	area = models.IntegerField(default=0)
+
+	def __unicode__(self):
+		return self.name
 
 class Country(models.Model):
 	continent = models.ForeignKey(Continent)
 	name = models.CharField(max_length=50)
 	population = models.IntegerField(default=0)
 
+	def __unicode__(self):
+		return self.name
+
 class Message(models.Model):
 	country = models.OneToOneField(Country)
 	author = models.CharField(max_length=50)
 	message = models.CharField(max_length=500)
+
+	def __unicode__(self):
+		return self.country + self.author
