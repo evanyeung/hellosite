6c4613641fbcf29193a6b9d3f9d8e631a09e7233
Evan Yeung <evan.yeung1@gmail.com>
2014-01-24 09:31:38 -0500

    can send a post request. callback not working

--- helloworld/static/helloworld/country_comment.js
+++ helloworld/static/helloworld/country_comment.js
@@ -1,8 +1,28 @@
 $( document ).ready(function() {
+	//for csrf
+	var csrftoken = $.cookie('csrftoken');
+	function csrfSafeMethod(method) {
+	// these HTTP methods do not require CSRF protection
+	return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+	}
+	$.ajaxSetup({
+		//contentType: "application/x-www-form-urlencoded",
+		success: function(){alert("success"); console.log("success");},
+		error: function(){alert("error"); console.log("success");},
+		complete: function(){alert("complete"); console.log("complete");},
+		crossDomain: false, // obviates need for sameOrigin test
+		beforeSend: function(xhr, settings) {
+			if (!csrfSafeMethod(settings.type)) {
+				xhr.setRequestHeader("X-CSRFToken", csrftoken);
+			}
+		}
+	});
+
 	$("#comment-submit").click(function() {
 		alert("clicked");
 		//var url = 'localhost:8000/helloworld/ajax/';
-		$.post('http://localhost:8000/helloworld/ajax/', function( data, status ) {
+		$.post('http://localhost:8000/helloworld/ajax/',{'name': 'country'}, function( data, status ) {
+			console.log(status)
 			alert("called"); });
 			/*data = json.parse( data )
 			var message_html = ""
